23cb7d9c2e8765d59e23d34d4d0c054f3bf3bfba
ayurani <ayra.purwantika@gmail.com>
2017-06-08 19:24:32 +0700

    versi 1.0.0

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '787918004719392',
+      xfbml      : true,
+      version    : 'v2.9'
+    });
+    FB.AppEvents.logPageView();
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
+</head>
+<body>
+
+	
+	<div id="fb-root"></div>
+	
+	
+	<div class="fb-like"
+		data-href="https://www.facebook.com/mysjaree/"
+		data-layout="standard"
+		data-action="like"
+		data-show-faces="true">
+	</div>
+	
+	</body>
+	</html>
